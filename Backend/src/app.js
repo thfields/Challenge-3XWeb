ad3ad36768e8a11a4b388935211601e7e2fca203
Thiago Campos <thiagoabcfc@hotmail.com>
2024-06-18 14:06:01 -0300

    deploy: vercel 1.2 restringido dominio local

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173' // substitua por seu próprio domínio
+}));
 app.use(routes);
 
 export default app;
